da638ec21227935f3d5080a00865ffd81fe5f3d8
Brandon <90708663+BrandonW1998@users.noreply.github.com>
2024-06-27 17:46:53 -0400

    updated homepage w/ photo and stats

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,5 +1,67 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
 const Photo = () => {
-  return <div>Photo</div>;
+  return (
+    <div className="w-full h-full relative">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+        }}
+      >
+        {/* Image */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="w-[300px] h-[300px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
+        >
+          <Image
+            src="/assets/self-photo-1.png"
+            priority
+            quality={100}
+            fill
+            alt=""
+            className="object-contain"
+          ></Image>
+        </motion.div>
+
+        {/* Circle */}
+        <motion.svg
+          className="w-[300px] h-[300px] xl:w-[498px] xl:h-[498px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="6"
+            strokeLinecap="round"
+            strokeLinjoin="round"
+            initial={{ strokeDasharray: "100 50 50 100" }}
+            animate={{
+              strokeDasharray: ["0 50 100 50", "100 50 0 50", "50 100 0 100"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          ></motion.circle>
+        </motion.svg>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Photo;
